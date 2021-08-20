4f3d346a30ce2d67c96d31658b443f921863a940
unknown <juren528@gmail.com>
2021-08-20 15:53:12 +0900

    a

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/express-app
 
+source /home/ec2-user/.bash_profile
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/baxh_completion"
