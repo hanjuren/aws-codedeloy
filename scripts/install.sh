80eca8e1cc87e20913bd816cb08f22a168a60e08
unknown <juren528@gmail.com>
2021-08-20 14:28:31 +0900

    code

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# download node and npm
-curl curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 14.*
-
 DIR="/home/ec2-user/express-app"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Stopping node servers
 echo "Stopping any existing node servers"
-pkill node
+pm2 stop nodeapp
