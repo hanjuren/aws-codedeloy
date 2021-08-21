1a849f519fab97491fc033d7fa85b4f1171b8d52
unknown <juren528@gmail.com>
2021-08-21 15:31:21 +0900

    createev

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -8,4 +8,9 @@ export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+cd secret
+mv .env ../
+cd ../
+rm -rf secret
+
 npm start
