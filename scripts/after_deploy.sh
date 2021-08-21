5124f3554f1f8a766df14b09cc8d198be0cca822
unknown <juren528@gmail.com>
2021-08-21 13:54:24 +0900

    path

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -4,7 +4,8 @@ REPOSITORY=/home/ec2-user/express-app
 cd $REPOSITORY
 
 # nvm 환경변수 등록
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 npm start
